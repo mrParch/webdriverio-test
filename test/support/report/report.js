4c235f905f383c37d363a64255fb0741eca5c2be
Maksim Parcheuski <maksim_parcheuski@epam.com>
2020-06-17 08:04:35 +0300

    refactor: delete unused modules

--- test/support/report/report.js
+++ test/support/report/report.js
@@ -1,9 +1,7 @@
-const cucumberJson = require ('wdio-cucumberjs-json-reporter').default;
+const cucumberJson = require('wdio-cucumberjs-json-reporter').default;
+const { After, Status } = require('cucumber');
 
-
-const {After, Status} = require('cucumber');
-
-After((scenarioResult)=>{
+After((scenarioResult) => {
     if (scenarioResult.result.status === Status.FAILED) {
         cucumberJson.attach(browser.takeScreenshot(), 'image/png');
     }
